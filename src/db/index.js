66fb8704eac8efe54b19ec2b3fa886ab9d739f08
Rushi <rushiwadekar0@gmail.com>
2025-05-04 16:23:16 +0530

    removed env sample file & removed URL from index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,7 @@ import { DB_NAME } from "../constant.js";
 
 const connectDB = async () =>{
     try {
-        console.log(`mongodb+srv://rushiWadekar:rushi7982@cluster1.npui7q9.mongodb.net/${DB_NAME}`,"<<<<<<<<<<<<y")
         const connectionObj= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        // const connectionObj= await mongoose.connect(`mongodb+srv://rushiWadekar:rushi7982@cluster1.npui7q9.mongodb.net/${DB_NAME}`)
         console.log(`MongoDB connected successfully...!! ${connectionObj.Connection}`);
         
     } catch (error) {
